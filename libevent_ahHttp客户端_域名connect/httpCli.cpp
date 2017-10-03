be624a3b431fcd05ce97ab6ffc1a909bb02a02ea
hanlon <1198432354@qq.com>
2017-10-03 15:36:00 +0800

    1.bufferevent_socket_connect_hostname 可以连接到服务器域名 需要配合evdns_base 2.evbuffer_remove 可以将evbuffer的数据拷贝到buffer中 读回调中要读取完所有数据再返回 3.evbuffer 写用 evbuffer_add_printf bufferevent_write bufferevent_write_buffer 读 evbuffer_remove bufferevent_read_buffer  获得数据长度evbuffer_get_length

---
+++ libevent_ahHttp客户端_域名connect/httpCli.cpp
@@ -0,0 +1,67 @@
+/*
+
+g++ httpCli.cpp -o httpCli -levent 
+
+
+*/ 
+
+ 
+#include <event2/dns.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/event.h>
+
+#include <stdio.h>
+
+void readcb(struct bufferevent *bev, void *ptr)
+{
+    char buf[1024];
+    int n;
+    struct evbuffer *input = bufferevent_get_input(bev);
+    while ((n = evbuffer_remove(input, buf, sizeof(buf))) > 0) {// 一直读到没有
+        fwrite(buf, 1, n, stdout); // 写到命令行输出
+    }
+}
+
+void eventcb(struct bufferevent *bev, short events, void *ptr)
+{
+    if (events & BEV_EVENT_CONNECTED) {
+         printf("Connect okay.\n");
+    } else if (events & (BEV_EVENT_ERROR|BEV_EVENT_EOF)) { // 遇到错误 或者对方关闭
+         struct event_base *base = ( struct event_base *)ptr;
+         if (events & BEV_EVENT_ERROR) {
+                 int err = bufferevent_socket_get_dns_error(bev);
+                 if (err)
+                         printf("DNS error: %s\n", evutil_gai_strerror(err));
+         }
+         printf("Closing\n");
+         bufferevent_free(bev);
+         event_base_loopexit(base, NULL);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    struct event_base *base;
+    struct evdns_base *dns_base;
+    struct bufferevent *bev;
+
+    if (argc != 3) {
+        printf("Trivial HTTP 0.x client\n"
+               "Syntax: %s [hostname] [resource]\n"
+               "Example: %s www.google.com /\n",argv[0],argv[0]);
+        return 1;
+    }
+
+    base = event_base_new();
+    dns_base = evdns_base_new(base, 1);
+
+    bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+    bufferevent_setcb(bev, readcb, NULL, eventcb, base);
+    bufferevent_enable(bev, EV_READ|EV_WRITE);
+    evbuffer_add_printf(bufferevent_get_output(bev), "GET %s\r\n", argv[2]); 		//	往eventbuffer printf格式化数据
+    bufferevent_socket_connect_hostname(  bev, dns_base/*不提供的话会一直阻塞*/, AF_UNSPEC, argv[1], 80);	//	解析域名并且连接 
+    event_base_dispatch(base);
+    return 0;
+}
